a74ba19c6832be523e5b886f67d5d4e8f1c5f2f3
oxi1224 <oxi1224@o2.pl>
2024-09-16 17:38:06 +0200

    Skończenie zadania 4

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+
+Console.WriteLine("Podaj kwotę w PLN:");
+string? input = Console.ReadLine();
+if (input == null) {
+  return;
+}
+HttpClient client = new();
+HttpResponseMessage res = await client.GetAsync("https://api.nbp.pl/api/exchangerates/rates/a/usd/");
+
+string content = await res.Content.ReadAsStringAsync();
+Kurs? kurs = JsonSerializer.Deserialize<Kurs>(content);
+if (kurs == null) {
+  Console.WriteLine("Nie udało się pobrać kursu USD");
+  return;
+}
+
+float kwota = float.Parse(input.Trim());
+Console.WriteLine($"Kwota w USD: {Math.Round(kwota / kurs.rates[0].mid, 2).ToString("N2")}");
+
+public class Kurs {
+  public string code { get; set; }
+  public string currency { get; set; }
+  public List<KursRates> rates { get; set; }
+  public string table { get; set; }
+}
+
+public class KursRates {
+  public string no { get; set; }
+  public string effectiveDate { get; set; }
+  public float mid { get; set; }
+}
